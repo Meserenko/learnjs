d59e4eb164e20c7c51a4d3cb0c9148767fff7aac
Oleg Meserenko <meserenko.oleg@gmail.com>
2020-09-12 15:01:46 +0300

    homework

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ if (a===b) {
 // 4. Запитайтесь в юзера радіус кола та виведіть його площу.(число пі)
 /*
 
-let radius = +prompt('what is the radius of the circle?');
+let radius = +prompt('What is the radius of the circle?');
 const PI = 3.14;
 let area = PI * (Math.pow(radius, 2));
 alert(`Area of the circle is ${area}`);*/
@@ -40,3 +40,55 @@ alert(`Area of the circle is ${area}`);*/
 //-----------------------------------------------------------------------------------------------------------
 
 // 5. Водій їде з середньою швидкістю 50 км в годину. Запитайтесь у нього скільки ще йому необхідно часу їхати, та виведіть відстань.
+
+/*
+let time = +prompt("Скільки годин залишилось їхати?")
+const speed = 50;
+let distance = time * speed;
+alert(`Вам залишилось їхати ${distance} км`);
+
+
+*/
+
+
+
+
+
+//6. Реалізуємо конвертер кілометрів в милі і навпаки =)
+/* km to miles
+let kilometers = +prompt("Converter km to miles. Write km");
+let converter = kilometers / 1.609;
+let miles = converter;
+alert(`${kilometers} km = ${miles.toFixed(3)} miles`);*/
+
+/* Miles to km
+let miles = +prompt("Converter miles to km. Write miles");
+let converter = miles * 1.609;
+let kilometers = converter;
+alert(`${miles} miles = ${kilometers.toFixed(3)} km`);
+*/
+
+
+
+
+
+
+
+// 7. Юзер вводить скільки коштів на його картці та ціну одного літра бензину. Виводимо скільки бензину він може купити та скільки грошей у нього залишиться.
+
+let money = +prompt("Введіть кількість грн на вашій картці");
+const benzinPrice = +prompt("Введіть ціну одного літра бензину");
+let benzin = money / benzinPrice;
+let rest = money - (Math.floor(benzin)*benzinPrice);
+
+let success = confirm(`Ви зможете купити ${Math.floor(benzin)} л бензину. Та у вас залишиться ${rest} грн. Ви згодні?`);
+if (success == true) {
+    alert(`Ви успішно купили ${benzin} л бензину`)
+} else {
+    let yourWish = +prompt("Тоді скільки літрів бензину ви бажаєте купити?");
+    let currentLiter = benzin - yourWish;
+    let bank = currentLiter * benzinPrice;
+    alert(`Тоді у вас залишиться ${bank} грн`);
+}
+
+
